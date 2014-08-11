c8df30c0b62ff60fab518129fa6040d9c64c9464
slabounty <slabounty@gmail.com>
2014-08-11 16:19:21 -0700

    Finished Layout Start Signup
    Finished the layout and started the user new for the signup page.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ DotGridRails::Application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get'
+
+  get "users/new"
 end
