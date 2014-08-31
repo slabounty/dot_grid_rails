e906d17543a3d5b972c40152f2a13d50b42620c8
Scott LaBounty <slabounty@gmail.com>
2014-08-31 14:19:49 -0700

    Finish Sign In

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to Dot Grid!"
       redirect_to @user
     else
       render 'new'
